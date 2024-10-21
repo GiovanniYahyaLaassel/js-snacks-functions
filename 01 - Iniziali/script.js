372a1c9f7d3248bc0ba50dc017e63e7bac10f3b8
Laassel <lassel.giovanni@gmail.com>
2024-10-21 16:41:32 +0200

    ex-iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,9 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitials(array) {
+    const initials = [];
+    //Inizio il ciclo for 
+    for(let i = 0; i < array.length; i++) {
+        //prendo la prima lettera di ogni nome
+        initials.push(array[i][0])
+    }
+    return initials
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = getInitials(names);
+console.log(result);
 
 
 
